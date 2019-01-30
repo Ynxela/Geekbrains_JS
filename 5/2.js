f0ad167b4cd79289ddd3ec7f2096ad3ebe5af719
Aleksandr Sofronov <42114787+Ynxela@users.noreply.github.com>
2019-01-30 13:28:05 +0300

    Сделал 2 задание в 5 ДЗ

--- 5/2.js
+++ 5/2.js
@@ -21,6 +21,10 @@ const chess = {
     initCells() {
         const rows = [0, 8, 7, 6, 5, 4, 3, 2, 1, 0];
         const cols = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
+        const whiteChess1 = [0, '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙', 0];
+        const whiteChess2 = [0, '♖', '♘', '♗', '♔', '♕', '♗', '♘', '♖', 0];
+        const blackChess1 = [0, '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟', 0];
+        const blackChess2 = [0, '♜', '♞', '♝', '♚', '♛', '♝', '♞', '♜', 0];
 
         for (let row = 0; row < this.settings.rowsCount; row++) {
             // Создаем новую строку.
@@ -48,6 +52,22 @@ const chess = {
 
                 if (rows[row] !== 0 && cols[col] !== 0 && col % 2 !== 0 && row % 2 === 0) {
                     tdElem.style.backgroundColor = this.settings.blackField;
+                }
+                
+                if (rows[row] === 7 && whiteChess1[col] !== 0) {
+                    tdElem.innerHTML = whiteChess1[col];
+                }
+                
+                if (rows[row] === 8 && whiteChess1[col] !== 0) {
+                    tdElem.innerHTML = whiteChess2[col];
+                }
+                
+                if (rows[row] === 2 && blackChess1[col] !== 0) {
+                    tdElem.innerHTML = blackChess1[col];
+                }
+                
+                if (rows[row] === 1 && whiteChess1[col] !== 0) {
+                    tdElem.innerHTML = blackChess2[col];
                 }
             }
         }
